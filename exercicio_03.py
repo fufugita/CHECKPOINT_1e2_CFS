7d512e65e953ead5b765e1f80e74cee619d3b9fb
fugita <81188207+fufugita@users.noreply.github.com>
2022-05-01 18:47:47 -0300

    Create exercicio_03.py
    Exercício 3 do CHECKPOINT de CFS

---
+++ exercicio_03.py
@@ -0,0 +1,28 @@
+# item a)
+def vel_media(d, t):
+  vm = d / t
+  return d, t, vm
+
+# item b)
+def soma_dois():
+  x = int(input("Digite o primeiro número: "))
+  y = int(input("Digite o segundo número: "))
+  return x + y
+
+# item c)
+def sub_dois():
+  x = int(input("Digite o primeiro número: "))
+  y = int(input("Digite o segundo número: "))
+  return x - y
+
+# item d)
+def mult_dois():
+  x = int(input("Digite o primeiro número: "))
+  y = int(input("Digite o segundo número: "))
+  return x * y
+
+# item e)
+def div_dois():
+  x = int(input("Digite o primeiro número: "))
+  y = int(input("Digite o segundo número: "))
+  return x / y
