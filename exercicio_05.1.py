82604bde726525859b9d9b60b2c17d023bf41da6
fugita <81188207+fufugita@users.noreply.github.com>
2022-05-02 11:25:49 -0300

    Update exercicio_05.1.py
    pequenas alterações

--- exercicio_05.1.py
+++ exercicio_05.1.py
@@ -3,18 +3,20 @@ from random import randrange
 def palavra_secreta():
     palavras = ["Bigode", "Cadeira", "Doce", "Mola", "Assobio", "Sobrancelhas", "Lamente", "Pimenta", "Capacete", "Limpo", "Estanho", "Mala", "Soldado", "Torneira", "Dinheiro", "Peixe", "Garrafa", "Joelho", "Melancia", "Ovos"]
 
-    # Escolha aleatória de um número entre 0 e comprimento da lista
+    # Escolha aleatória de um número entre 0 e comprimento da lista de palavras
     num = randrange(0, len(palavras))
     ps = palavras[num].upper()
   
     return ps
 
-
+# Função que preenche uma lista com "_" as letras de acordo com a quantidade de letras da palavra secreta
 def censura(palavra_secreta):
+  
   cens = ["_" for letra in palavra_secreta]
   
   return cens
 
+# Função que marca acertos do usuário substituindo os "_" pelas letras corretas
 def marca_acerto(letra, letras_acertadas, palavra_secreta):
   i = 0
   # Loop que passa em cada letra da palavra secreta
@@ -27,10 +29,11 @@ def marca_acerto(letra, letras_acertadas, palavra_secreta):
 
 
 def jogo(palavra_secreta):
-  
+
+  # Seleção e Censura da palavra secreta aleatória
   resposta = censura(palavra_secreta)
-  print(palavra_secreta)
-  
+
+  # Condições para acabar o jogo
   acertou = False
   enforcou = False
 
@@ -42,27 +45,37 @@ def jogo(palavra_secreta):
   print("Tente acertar a palavra de {0} letras".format(len(resposta)))
   print(resposta)
 
+  # Loop que verifica se o jogo não acabou ainda
   while not acertou and not enforcou:
-    
+
+    # Input do chute do usuário
     chute = input("\nDigita uma letra: ").upper()
+
+    # Condição que verifica se o chute está correto
     if chute in palavra_secreta:
-      
+
+      # Chama a função que substitui os "_" pelo chute correto
       marca_acerto(chute, resposta, palavra_secreta)
+      # Conta quantos "_" restam ainda
       faltando = resposta.count('_')
+      
       print(resposta)
       print("Letras erradas: ", lista_erros)
 
     else:
+      # Condição que adiciona as letras erradas em uma lista
       print("A letra '{0}' não está na palavra.\n".format(chute))
       if chute not in lista_erros:
         lista_erros.append(chute)
   
         erros += 1
+        
         print(resposta)
         print("\nLetras erradas: ", lista_erros)
         print("\nAinda faltam {0} letras.".format(faltando))
         print("Você tem {0} tentativas.".format(tentativas-erros))
 
+    # Verifica as condições para acabar o jogo sendo 6 o número de tentativas caso erre
     if erros == 5:
       enforcou = True
     elif "_" not in resposta:
